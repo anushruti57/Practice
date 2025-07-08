f360278384064b92d7698a6a05f5719a6c355d58
anushruti57 <anushruti8726@gmail.com>
2025-07-08 13:05:07 +0530

    Tic-Tac-Toe Game

---
+++ Desktop/Home/tic-tac-toe/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Game from './components/Game';
+
+
+function App() {
+  return(
+    <div>
+        <h1>Tic-Tac-Toe Game</h1>
+        <Game/>
+    </div>
+  );  
+}
+
+
+export default App;
